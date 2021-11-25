b1210f4660a531bcb7be08d57c421b91fb18a1b9
Joaquin Guzman <githubjoaquin@gmail.com>
2021-11-25 09:26:14 +0800

    juice: Bring Up Pixel Experience!!

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,17 +6,16 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common Pixel Experience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_GAPPS_ARCH := arm64
 
 # Bootanimation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2340
+TARGET_BOOTANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -25,6 +24,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
-
-# Havoc Official
-HAVOC_BUILD_TYPE := Official
